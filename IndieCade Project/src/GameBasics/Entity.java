aaea8bf75db44bd1a5d9247e969910daa9291266
c-grenade <c.grande@cox.net>
2017-01-18 14:34:24 -0700

    Yay

--- IndieCade Project/src/GameBasics/Entity.java
+++ IndieCade Project/src/GameBasics/Entity.java
@@ -1,61 +1,48 @@
 package GameBasics;
 
+import java.io.Serializable;
+
 import org.newdawn.slick.Graphics;
 
 import Geo.Quad;
 import Render.AnimationSet;
 import Render.BasicImage;
 
-public abstract class Entity 
+public abstract class Entity implements Serializable
 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1335939607570570293L;
+	
+	//Horizontal movement
 	float x;
-	float y;
 	float Vx;
 	float Vy;
 	float Ax;
 	float Ay;
+	float acceleration;
 	
-	Quad Hitbox;
+	//Vertical movement
+	float y;
+	float jumpV;
+	int jump;
+	int maxJumps;
+	long jumpTick;
+	boolean jumping;
 	
-	float height;
-	float width;
-	AnimationSet Base;
-	public Entity()
-	{
-		
-	}
+	//Combat statistics
+	float maxHealth;
+	float health;
+	float damage;
+	float defense;
+	float speed;
+	float tenacity;
+	float basespeed;
 	
-	public void init()
-	{
-		width = ((BasicImage)Base.getSet().get(0)).getImage().getWidth();
-		height = ((BasicImage)Base.getSet().get(0)).getImage().getHeight();
-		Hitbox = new Quad(x, y, width, height);
-		
-		//Finish initializing
-	}
+	//Combat values
+	float Stun;
+	float Speed;
 	
-	public Entity setLocation(float xa, float ya)
-	{
-		x = xa;
-		y = ya;
-		
-		
-		
-		return this;
-	}
 	
-	public void update()
-	{
-		
-	}
-	
-	public void render(Graphics g)
-	{
-		
-	}
-	
-	public Quad getHitbox()
-	{
-		return Hitbox;
-	}
 }
