33a73fcd4eb8fe3094554d12550f4957c1121585
c-grenade <c.grande@cox.net>
2017-01-19 19:20:19 -0700

    EnemyEntity update

--- IndieCade Project/src/GameBasics/Entity.java
+++ IndieCade Project/src/GameBasics/Entity.java
@@ -3,10 +3,15 @@ package GameBasics;
 import java.io.Serializable;
 
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
+import BoneStructure.BoneStructure;
 import Geo.Quad;
+import Main.Config;
+import Map.Map;
 import Render.AnimationSet;
 import Render.BasicImage;
+import Tiles.Tile;
 
 public abstract class Entity implements Serializable
 {
@@ -14,6 +19,8 @@ public abstract class Entity implements Serializable
 	 * 
 	 */
 	private static final long serialVersionUID = -1335939607570570293L;
+	
+	boolean paused;
 	
 	//Horizontal movement
 	float x;
@@ -27,22 +34,225 @@ public abstract class Entity implements Serializable
 	float y;
 	float jumpV;
 	int jump;
-	int maxJumps;
+	int maxjump;
 	long jumpTick;
 	boolean jumping;
 	
 	//Combat statistics
+	BoneStructure body;
 	float maxHealth;
 	float health;
 	float damage;
 	float defense;
 	float speed;
 	float tenacity;
-	float basespeed;
 	
 	//Combat values
 	float Stun;
 	float Speed;
+	
+	//World
+	Map map;
+	
+	//Renders
+	float width;
+	float height;
+	public Quad Hitbox;
+	Quad Screen;
+	Level level;
+	
+	public Entity(float x, float y, float speed)
+	{
+		
+		paused = false;
+		
+		this.x = x;
+		this.y = y;
+		jumpV = -10F;
+		jump = 0;
+		maxjump = 2;
+		jumpTick = System.currentTimeMillis();
+		jumping = false;
+		
+		Vx = 0;
+		Vy = 0;
+		
+		Ax = 0;
+		Ay = 0.5F;
+		acceleration = 0.5F;
+		
+		width = 64;
+		height = 64;
+		
+		Hitbox = new Quad(x, y, width, height);
+		Screen = new Quad(x, y, Config.WIDTH, Config.HEIGHT);
+		
+		maxHealth = 100;
+		health = maxHealth;
+		defense = 5;
+		damage = 5;
+		tenacity = 0;
+		this.speed = 5;
+	}
+	public void setMap(Map map)
+	{
+		this.map = map;
+	}
+	
+	public void Physics()
+	{
+		Vx += Ax;
+		
+		boolean CollisionX = false;
+		boolean CollisionY = false;
+		
+		for(Tile T : map.getTiles())
+		{
+			if(T.getCollidable() && T.getHitbox().checkQuad(new Quad(x + Vx + Ax, y, width, height)))
+			{
+				CollisionX = true;
+				T.nextTo = true;
+			}else
+			{
+				T.nextTo = false;
+			}
+			
+			if(T.getCollidable() && T.getHitbox().checkQuad(new Quad(x, y + Vy + Ay, width, height)))
+			{
+				CollisionY = true;
+				T.on = true;
+			}else
+			{
+				T.on = false;
+			}
+		}
+		
+		if(CollisionX)
+		{
+			Vx = 0;
+		}
+		
+		if(CollisionY)
+		{
+			jumping = false;
+			if(jump < maxjump && Vy > 0)
+			{
+				jump ++;
+			}
+			Vy = 0;
+		}else
+		{
+			Vy += Ay;
+			if(!jumping)
+			{
+				jump = 0;
+			}
+		}
+		
+	}
+	
+	
+	public void update()
+	{
+		if(!paused)
+		{
+			Physics();
+			
+			if(Math.abs(Vx) < map.getCurrentTile(x, y + 50).getFriction())
+			{
+				Vx = 0;
+			}
+			
+			if(health <= 0)
+			{
+				die();
+			}
+		}
+		
+		map.shift(-Vx, 0);
+		map.shift(0, -Vy);
+		
+		Hitbox.changeDimensions(x, y, width, height);
+		Screen.changeDimensions(x, y, Config.WIDTH, Config.HEIGHT);
+	}
+	
+	public void render(Graphics g) throws SlickException
+	{
+		Hitbox.render(g);
+		Screen.render(g);
+	}
+	
+	public void jump()
+	{
+		Vy = jumpV;
+		jump --;
+		jumping = true;
+		
+		jumpTick = System.currentTimeMillis();
+	}
+	
+	public void giveItem(Item item)
+	{
+		//needed?
+		//Inventory.add(item);
+	}
+	
+	public void pause()
+	{
+		paused = true;
+	}
+	
+	public void unpause()
+	{
+		paused = false;
+	}
+	
+	public float getX()
+	{
+		return x;
+	}
+	
+	public float getY()
+	{
+		return y;
+	}
+	
+	public float getVx()
+	{
+		return Vx;
+	}
+	
+	public float getVy()
+	{
+		return Vy;
+	}
+	
+	public Map getMap()
+	{
+		return map;
+	}
+	
+	public BoneStructure getBody()
+	{
+		return body;
+	}
+	
+	//...
+	public void damage(int damage)
+	{
+		health -= damage;
+	}
+	
+	public void die()
+	{
+		for(int i = 0; i < level.getEntities().size(); i ++)
+		{
+			if(level.getEntities().get(i) == this)
+			{
+				level.getEntities().remove(i);
+			}
+		}
+	}
 	
 	
 }
